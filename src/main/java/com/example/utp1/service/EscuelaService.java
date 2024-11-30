7cf8ba5706289c2f3822b9cde47e0b6aebf88ee9
gxjohan <johamax@gmail.com>
2024-11-30 09:17:23 -0500

    v2.5

--- src/main/java/com/example/utp1/service/EscuelaService.java
+++ src/main/java/com/example/utp1/service/EscuelaService.java
@@ -13,4 +13,37 @@ public class EscuelaService {
     @Autowired
     private EscuelaRepository escuelaRepository;
 
+    //Método para obtener todas las escuelas en una lista
+    public List<Escuela> getAllEscuelas() {
+        return escuelaRepository.findAll();
+    }
+    //Método para obtener una escuela por su id
+    public Escuela getEscuelaById(Long id) {
+        return escuelaRepository.findById(id).orElse(null);
+    }
+
+    //Método para crear/registrar una escuela
+    public Escuela createEscuela(Escuela escuela) {
+        return escuelaRepository.save(escuela);
+    }
+
+    //Método para editar una escuela
+    public Escuela actualizarEscuela(Long id,Escuela escuela) {
+        Escuela escuelaActual = escuelaRepository.findById(id).orElse(null);
+        if(escuelaActual!=null) {
+            escuelaActual.setNombre(escuela.getNombre());
+            escuelaActual.setCreditos(escuela.getCreditos());
+            escuelaActual.setNumEstudiantes(escuela.getNumEstudiantes());
+            return escuelaRepository.save(escuelaActual);
+        }else{
+            return null;
+        }
+
+    }
+
+    //Método para eliminar a una escuela sabiendo su id
+    public void eliminarEscuela(Long id) {
+        escuelaRepository.deleteById(id);
+    }
+
 }
